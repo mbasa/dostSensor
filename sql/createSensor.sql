5234b8ce9e291ba981463093d43fb905b1d17f8c
Mario Basa <mario.basa@gmail.com>
2013-08-28 18:35:35 +0800

    added new tables

--- sql/createSensor.sql
+++ sql/createSensor.sql
@@ -1,10 +1,50 @@
-create table sensor (
-    id    serial,
-    name  text,
-    time  timestamp,
-    value float,
-    lon   float,
-    lat   float,
-    the_geom geometry('POINT',4326),
-    PRIMARY KEY ( name,time )
-);
+    create table asg (
+        id    serial,
+        name  text,
+        time  timestamp,
+        value float,
+        water_level_change float,
+        time_difference    float,
+        lon   float,
+        lat   float,
+        the_geom geometry('POINT',4326),
+        PRIMARY KEY ( name,time )
+    );
+
+    create table td (
+        id    serial,
+        name  text,
+        time  timestamp,
+        value float,
+        water_level_change float,
+        time_difference    float,
+        lon   float,
+        lat   float,
+        the_geom geometry('POINT',4326),
+        PRIMARY KEY ( name,time )
+    );
+
+    create table arg (
+        id    serial,
+        name  text,
+        time  timestamp,
+        value float,
+        lon   float,
+        lat   float,
+        the_geom geometry('POINT',4326),
+        PRIMARY KEY ( name,time )
+    );
+
+    create table aws (
+        id    serial,
+        name  text,
+        time  timestamp,
+        temp  float,
+        humi  float,
+        pres  float,
+        rain  float,
+        lon   float,
+        lat   float,
+        the_geom geometry('POINT',4326),
+        PRIMARY KEY ( name,time )
+    );
