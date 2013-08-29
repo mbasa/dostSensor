1ecf31577406dff04d5f1bbfcba1b7cf69816c5e
Mario Basa <mario.basa@gmail.com>
2013-08-29 09:36:41 +0800

    added create view for latest data

--- sql/createSensor.sql
+++ sql/createSensor.sql
@@ -48,3 +48,25 @@
         the_geom geometry('POINT',4326),
         PRIMARY KEY ( name,time )
     );
+
+    create view asg_latest as  
+        select a.* from asg as a inner join 
+            (select  name,max(time) as time from asg group by name) as b 
+            on a.name = b.name and a.time = b.time order by a.time desc;
+            
+    create view arg_latest as  
+        select a.* from arg as a inner join 
+            (select  name,max(time) as time from arg group by name) as b 
+            on a.name = b.name and a.time = b.time order by a.time desc;     
+            
+    create view aws_latest as  
+        select a.* from aws as a inner join 
+            (select  name,max(time) as time from aws group by name) as b 
+            on a.name = b.name and a.time = b.time order by a.time desc;
+        
+    create view td_latest as  
+        select a.* from td as a inner join 
+            (select  name,max(time) as time from td group by name) as b 
+            on a.name = b.name and a.time = b.time order by a.time desc;        
+        
+        
